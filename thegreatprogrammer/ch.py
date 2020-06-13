731665a2e971d200c02035800f847eaf69b4f83c
Vaibhav Pareek(Eternity) <vaibhavpareekvp7@gmail.com>
2020-06-13 14:20:21 +0530

    Update ch.py

--- thegreatprogrammer/ch.py
+++ thegreatprogrammer/ch.py
@@ -1,7 +1,7 @@
 #use this script if you want to great more than 4000 links in worlist, i have created chall for 4000.
 
 ch=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-pro = ["cpp","python","js","css"]
+pro = ["cpp","python","js","css","php"]
 import random
 fp = open("wordlist.txt","w+")
 for i in range(0,3000):
